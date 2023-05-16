0f7c1ba19452b884de96c860610adc6e00968492
AlexProv1987 <alex.provenzano87@gmail.com>
2023-05-16 17:00:57 -0500

    more typos

--- src/http.js
+++ src/http.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 const axiosBaseURL = axios.create({
-    baseURL:"https://apitest.aprovtestdmns.com/.com/"
+    baseURL:"https://apitest.aprovtestdmns.com/"
 });
 export default axiosBaseURL
