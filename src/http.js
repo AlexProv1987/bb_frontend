e7f0e73a0fc35f0a9e0b463ecc0ecc0c0bddbb43
AlexProv1987 <alex.provenzano87@gmail.com>
2023-05-16 15:23:36 -0500

    fixed base

--- src/http.js
+++ src/http.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 const axiosBaseURL = axios.create({
-    baseURL:"https://base-ball-oracle-dev.us-east-1.elasticbeanstalk.com/"
+    baseURL:"http://base-ball-oracle-dev.us-east-1.elasticbeanstalk.com/"
 });
 export default axiosBaseURL
