ccff817bdac327f4733a1f6eb04f707fa67604b9
AlexProv1987 <96451610+AlexProv1987@users.noreply.github.com>
2023-05-22 12:13:32 -0500

    Dev (#9)
    * Resetting Dev Branch from Stable (#1)
    
    * trailing slash
    
    * reversion of node
    
    * removing not needed
    
    * removed for casing
    
    * name fix
    
    * https
    
    * removed default title
    
    * fixed base
    
    * proxy for now
    
    * https
    
    * more typos
    
    * fixed spelling
    
    * phone number clickable
    
    * svg migration fix to question input
    
    * Button active focus color fix
    
    * Added measure to ph
    
    * page-break attempt
    
    * Padding adjustment
    
    * Footer is now just the end of body content
    
    * syntax
    
    * svg sponors and cleanup
    
    * Changed to inches for the moment
    
    * Padding was still a bit light testing real phone
    
    * remove title camelcase
    
    * padding changes to question component
    
    * logo media query for small screen
    
    * Piped unit
    
    * Moved text start back
    
    * Load icons for calculators
    
    * unbolded form input
    
    * unit spacing
    
    * PageBreak Change
    
    * Screen Size adjustments
    
    * added triangle back

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -5,20 +5,23 @@ import Row from 'react-bootstrap/esm/Row';
 import Col from 'react-bootstrap/esm/Col';
 import GloveCalc from '../Components/GloveCalc';
 import LeagueFinder from '../Components/LeagueFinder';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faArrowDown} from '@fortawesome/free-solid-svg-icons'
+import pagebreak from '../imgs/pagebreak.png'
 const HomePage = () => {
     return (
         <Container fluid='true' className='main-container pt-2'>
             <Question />
-            <Container fluid='true' className='card-container h-100'>
-                <Container fluid='true' className='justify-content-center'>
-                    <Row className='text-center text-white pt-3'>
+            <Container fluid='true' className='justify-content-center'>
+                <Row>
+                    <img src={pagebreak} alt='page-break'></img>
+                </Row>
+                <Row className='text-center text-white pb-row'>
                         <Col>
                         <h3><b>Leagues & Calculators</b></h3>
                         </Col>
                     </Row>
-                </Container>
+            </Container>
+            <Container fluid='true' className='card-container h-100'>
+                <Container fluid='true' className='justify-content-center'>
                 <Container>
                     <Row className='justify-content-center'>
                         <Col xs={12} sm={12} md={12} lg={4} xl={4} className='card-column'>
@@ -33,6 +36,7 @@ const HomePage = () => {
                     </Row>
                 </Container>
             </Container>
+        </Container>
         </Container>
     );
 }
