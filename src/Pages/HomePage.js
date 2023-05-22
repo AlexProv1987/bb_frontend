f9760f9657bbdb53a37cb1a65b022ee325423db7
AlexProv1987 <96451610+AlexProv1987@users.noreply.github.com>
2023-05-22 10:24:13 -0500

    Dev (#7)
    * Resetting Dev Branch from Stable (#1)
    
    * trailing slash
    
    * reversion of node
    
    * removing not needed
    
    * removed for casing
    
    * name fix
    
    * https
    
    * removed default title
    
    * fixed base
    
    * proxy for now
    
    * https
    
    * more typos
    
    * fixed spelling
    
    * phone number clickable
    
    * svg migration fix to question input
    
    * Button active focus color fix
    
    * Added measure to ph
    
    * page-break attempt
    
    * Padding adjustment
    
    * Footer is now just the end of body content
    
    * syntax
    
    * svg sponors and cleanup
    
    * Changed to inches for the moment
    
    * Padding was still a bit light testing real phone
    
    * remove title camelcase
    
    * padding changes to question component
    
    * logo media query for small screen
    
    * Piped unit
    
    * Moved text start back
    
    * Load icons for calculators
    
    * unbolded form input
    
    * unit spacing
    
    * PageBreak Change

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -5,20 +5,24 @@ import Row from 'react-bootstrap/esm/Row';
 import Col from 'react-bootstrap/esm/Col';
 import GloveCalc from '../Components/GloveCalc';
 import LeagueFinder from '../Components/LeagueFinder';
-import PageBreak from '../imgs/pagebreak.png'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faArrowDown} from '@fortawesome/free-solid-svg-icons'
 const HomePage = () => {
     return (
         <Container fluid='true' className='main-container pt-2'>
             <Question />
-            <Container fluid='true' className='page-break-wrapper'>
-               <img src={PageBreak} alt='break'></img>
-            </Container>
             <Container fluid='true' className='card-container h-100'>
                 <Container fluid='true' className='justify-content-center'>
-                <Row className='text-center text-white'>
-                    <h4><b>Leagues & Calculators</b></h4>
-                </Row>
+                    <Row className='text-center pb-2 pt-2'>
+                        <Col>
+                        <FontAwesomeIcon icon={faArrowDown} size="2xl" style={{color: "#ffffff",}} />
+                        </Col>
+                    </Row>
+                    <Row className='text-center text-white'>
+                        <Col className='header-col'>
+                        <h3><b>Leagues & Calculators</b></h3>
+                        </Col>
+                    </Row>
                 </Container>
                 <Container>
                     <Row className='justify-content-center'>
