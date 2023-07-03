f68e85d17b165a684cb0cf78aa7c93877dd92db4
AlexProv1987 <alex.provenzano87@gmail.com>
2023-07-03 14:13:04 -0500

    footer text update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
         <Container fluid ='true' className='stuck-element'>
         <footer className="py-4 text-white stuck-element">
                 <Row xs='auto' className="justify-content-center">
-                   <a className="footer-link" target="_blank" rel="noopener noreferrer" href="https://procarnsolutions.com/">© 2021 Copyright: ProCarn Solutions</a>
+                   <a className="footer-link text-wrap" target="_blank" rel="noopener noreferrer" href="https://procarnsolutions.com/">Baseball Oracle℠ 2021 © ProCarn Solutions</a>
                 </Row>
         </footer>
         </Container>
