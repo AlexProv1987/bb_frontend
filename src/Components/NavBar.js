53bb616b1d2d3d30382e1bf498a9fa560ff4fd67
AlexProv1987 <96451610+AlexProv1987@users.noreply.github.com>
2023-08-01 15:37:53 -0500

    Dev (#40)
    * hamburger style change
    
    * load icon h/w props
    
    * font confusion
    
    * removed ph icon from nav
    
    * leagues Rule links

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import {Navbar, Nav} from 'react-bootstrap';
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { Link } from "react-router-dom";
@@ -23,6 +23,13 @@ const NavBar = () => {
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/" style={{color:'white', fontSize:'20px'}} className='nav-page-link'>Home</Nav.Link>
             <Nav.Link  as={Link} to="/about" style={{color:'white', fontSize:'20px'}} className='nav-page-link'>About</Nav.Link>
+            <NavDropdown title={<span className="my-auto nav-page-link" style={{color:'white', fontSize:'20px'}}>Rule Books</span>} id="basic-nav-dropdown" style={{fontSize:'20px'}}>
+              <NavDropdown.Item className='text-light bg-transparent book-item' target="_blank" rel="noopener noreferrer" href="https://www.littleleague.org/playing-rules/little-league-rulebook-app/">Little League International</NavDropdown.Item>
+              <NavDropdown.Item className='text-light bg-transparent book-item' target="_blank" rel="noopener noreferrer" href="https://www.usssa.com/baseball/baseball-rules">USSSA</NavDropdown.Item>
+              <NavDropdown.Item className='text-light bg-transparent book-item' target="_blank" rel="noopener noreferrer" href="https://www.nfhs.org/activities-sports/baseball/">NFHS</NavDropdown.Item>
+              <NavDropdown.Item className='text-light bg-transparent book-item' target="_blank" rel="noopener noreferrer" href="https://www.ncaapublications.com/c-66-baseball.aspx">NCAA</NavDropdown.Item>
+              <NavDropdown.Item className='text-light bg-transparent book-item' target="_blank" rel="noopener noreferrer" href="https://www.mlb.com/official-information">MLB</NavDropdown.Item>
+            </NavDropdown>
           </Nav>
           <Nav>
             <Row>
